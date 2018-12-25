44c15af1848bb1e7516f6b34457d1f6c40bf5058
CodingYuanLiu <lqyuan980413@163.com>
2018-12-25 16:34:01 +0800

    First commit

---
+++ tieba-master/src/components/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+	<footer>
+	</footer>
+</template>
+
+<style scoped>
+	footer{
+		background: #545c64;
+		color: white;
+		text-align: center;
+		padding: 10px 0;
+	}
+	footer a{
+		color: #ffd04b;
+	}
+</style>
